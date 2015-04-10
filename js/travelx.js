a3faf68184778f7505ec59943b58d7951fd4d18e
SevenStringArgs <robert.barlin@gmail.com>
2015-04-10 21:10:41 +0200

    Remove unuesd code

--- js/travelx.js
+++ js/travelx.js
@@ -1,10 +1,6 @@
 var SH = (function(){
 	var store = $.localStorage;
 	var eventPub = {};
-	// store.isEmpty(nsBusStopCache) && store.set(nsBusStopCache, {count: 0});
-	
-	var name = "It's name";
-
 
 	return {
 		get: function(ns){
