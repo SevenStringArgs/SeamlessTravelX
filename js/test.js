1aa1fbbc66181cc48ca9db2fc8ca47386bf6ca6e
SevenStringArgs <robert.barlin@gmail.com>
2015-04-10 21:21:33 +0200

    websocket added

--- js/test.js
+++ js/test.js
@@ -68,26 +68,26 @@ $(document).ready(function(){
 	registerMapEvents(map);
 
 
-	// var ws = new WebSocket();
+	var ws = new WebSocket('ws://stx-api-dev.mybluemix.net/mvdsubscriber/busService');
 
-	// ws.onopen = function()
- //     {
- //        // Web Socket is connected, send data using send()
- //        ws.send("Message to send");
- //        console.log("Message is sent...");
- //     };
+	ws.onopen = function()
+     {
+        // Web Socket is connected, send data using send()
+        // ws.send("Message to send");
+        console.log("Message is sent...");
+     };
 
- //     ws.onmessage = function (evt) 
- //     { 
- //        var received_msg = evt.data;
- //        console.log("Message is received...");
- //     };
+     ws.onmessage = function (evt) 
+     { 
+        var received_msg = evt.data;
+        console.log("Message is received...");
+     };
 
- //     ws.onclose = function()
- //     { 
- //        // websocket is closed.
- //        console.log("Connection is closed..."); 
- //     };
+     ws.onclose = function()
+     { 
+        // websocket is closed.
+        console.log("Connection is closed..."); 
+     };
 
 
 
