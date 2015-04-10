26c6dfbf914d19a2ec2ebf1fb85ed0606ab205d1
SevenStringArgs <robert.barlin@gmail.com>
2015-04-10 05:16:40 +0200

    Remove unused code in server.go

--- server.go
+++ server.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"net/http"
-	"fmt"
 	"os"
 	"log"
 )
@@ -23,21 +22,18 @@ func pageHandler(w http.ResponseWriter, r *http.Request){
 
 	if r.URL.Path == "/" {	
 		http.ServeFile(w, r, htmlRoot +  "/index.html")
-		fmt.Println("Getting index")
 		return
 	}
 
 	if _, err := os.Stat(htmlRoot + "/" + r.URL.Path[1:] + ".html"); err == nil {
 		http.ServeFile(w, r, htmlRoot + "/" + r.URL.Path[1:] + ".html")
-		fmt.Println("Getting " + r.URL.Path[1:]);
 		return
 	}
-	fmt.Println("Redirect Not Found")	
 	http.Redirect(w, r, "/404", 301)
 }
 
 func notFoundHandler(w http.ResponseWriter, r *http.Request){
-	fmt.Println("Not found")
+	log.Printf("%s not found.", r.URL.Path[1])
 	http.ServeFile(w, r, htmlRoot + "/404.html")
 }
 
@@ -45,18 +41,8 @@ func staticFileHandler(w http.ResponseWriter, r *http.Request){
 	http.ServeFile(w, r, r.URL.Path[1:])
 }
 
-func apiHandler(w http.ResponseWriter, r *http.Request){
-	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	// pollLatestDn()
-
-	fmt.Fprintf(w, `[{"Name":"Robert","Phone":"555-555-555"}, {"Name":"Harlad","Phone":"111-111-111"}, {"Name":"Joel","Phone":"222-222-222"}, {"Name":"Karl","Phone":"333-333-555"}]`)
-}
-
-
 func main(){
 	http.HandleFunc("/", pageHandler)
-	http.HandleFunc("/api/", apiHandler)
 	http.HandleFunc("/404", notFoundHandler)
 	http.HandleFunc("/js/", staticFileHandler)
 	http.HandleFunc("/css/", staticFileHandler)
