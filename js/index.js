ce40a283158efd68847a945dfe37e5a5a764d844
SevenStringArgs <robert.barlin@gmail.com>
2015-04-11 20:10:44 +0200

    reorganized index.js

--- js/index.js
+++ js/index.js
@@ -1,63 +1,12 @@
 $(document).ready(function(){
+	console.log('Ready');
 	var loops = [];
 	var loop;
 	var offers = OfferStore.get();
-	var selectedLine = undefined;
-	console.log(offers);
-
-	$.each(offers, function(key, offer){
-		var template = GraphicHelper.getTemplate('notificationItem', offer)
-		console.log(template);
-		var ul = $(document).find('#notificationDropDown');
-		$('#notificationDropDown').append(template);
-		var theLi = $('#notificationDropDown').find('#offerId-' + offer.id);
-		theLi.find('strong').text(offer.company);
-		theLi.find('.offerTag').text(offer.offer);
-	});
-
-	$('#testNotification').on('click', function(){
-		$('#dropDownImg').removeClass('glyphicon-bell');
-		$('#dropDownImg').addClass('glyphicon-info-sign');
-	});
-
-	$('#menu-toggle').click(function(e) {
-		e.preventDefault();
-		$("#wrapper").toggleClass("toggled");
-
-	});
-
-	$('#notificationToggle').on('click', function(){
-		$('#dropDownImg').addClass('glyphicon-bell');
-		$('#dropDownImg').removeClass('glyphicon-info-sign');
-
-		if($('.bus-search-input').css('display') === 'none')
-			$('.bus-search-input').fadeIn();
-		else 
-			$('.bus-search-input').fadeOut();
-
-	});
-
-	$('#notificationToggle').focusout(function(){
-		$('.bus-search-input').fadeIn();
-	});   
-
-	$('.bus-search-input').keypress(function(e){
-		if(e.which === 13){
-			alert(e);
-			console.log(e);
-			var busNr = $('.bus-search-input').val();
-			console.log(busNr);
-		}
-	});
-
-	console.log('Ready');
+	var selectedLine = 3;
 	var map = new Map();
-
-
 	var drawOnMap = { start: undefined, end: undefined };
-	
 	var bookmarker = new Bookmarker();
-
 	var drawCachedBusStops = function(busStops){
 		for(key in busStops){
 			drawNewBusStop(busStops[key]);
@@ -68,7 +17,7 @@ $(document).ready(function(){
 		console.log('****** Travel Changed *****')
 		console.log(travelObj);
 		map.setTravelObj(travelObj);
-	}
+	};
 
 	var removeBusStop = function(busStop){
 		map.removeBusStop(busStop);
@@ -113,6 +62,46 @@ $(document).ready(function(){
 		});
 	};
 
+	$('#testNotification').on('click', function(){
+		$('#dropDownImg').removeClass('glyphicon-bell');
+		$('#dropDownImg').addClass('glyphicon-info-sign');
+	});
+
+	$('#menu-toggle').click(function(e) {
+		e.preventDefault();
+		$("#wrapper").toggleClass("toggled");
+
+	});
+
+	$('#notificationToggle').on('click', function(){
+		$('#dropDownImg').addClass('glyphicon-bell');
+		$('#dropDownImg').removeClass('glyphicon-info-sign');
+
+		if($('.bus-search-input').css('display') === 'none')
+			$('.bus-search-input').fadeIn();
+		else 
+			$('.bus-search-input').fadeOut();
+
+	});
+
+	$('#notificationToggle').focusout(function(){
+		$('.bus-search-input').fadeIn();
+	});   
+
+	$('.bus-search-input').keypress(function(e){
+		if(e.which === 13){
+			alert(e);
+			console.log(e);
+			var busNr = $('.bus-search-input').val();
+			console.log(busNr);
+			if(isNaN(busNr) || !busNr){
+				selectedLine = undefined
+			} else {
+				selectedLine = busNr;
+			}
+		}
+	});	
+
 	$('#clearLocalStorage').on('click' ,function(){
 		BusStopStorage.clearStorage();
 	});
@@ -122,13 +111,14 @@ $(document).ready(function(){
 		$('.toggle-endPoint').toggleClass('notCurrent');
 	});
 
-
 	$('#port-nr').on('change', function() {
 
 		var newConf = TravelX.getConfig();
 		newConf.port = $(this).val();
 		TravelX.setConfig(newConf);
 	});
+
+	$('#port-nr').val(TravelX.getConfig().port);
 
 	$('#testApi').on('click', function(){
 		BusQuery.getRoute();
@@ -138,18 +128,26 @@ $(document).ready(function(){
 		$('.toggle-endPoint').toggleClass('notCurrent');
 	}
 
-	$('#port-nr').val(TravelX.getConfig().port);
+	$.each(offers, function(key, offer){
+		var template = GraphicHelper.getTemplate('notificationItem', offer)
+		console.log(template);
+		var ul = $(document).find('#notificationDropDown');
+		$('#notificationDropDown').append(template);
+		var theLi = $('#notificationDropDown').find('#offerId-' + offer.id);
+		theLi.find('strong').text(offer.company);
+		theLi.find('.offerTag').text(offer.offer);
+	});
 
+	BusHelper.onAddedBus(drawNewBus);
+	BusHelper.addTravelListener(travelListener);
 	BusStopStorage.subscribeBusStopAdded(drawNewBusStop);
 	BusStopStorage.subscribeBusStopRemoved(removeBusStop);
 	drawCachedBusStops(BusStopStorage.getAll());
 	registerMapEvents(map);
-
-	BusHelper.onAddedBus(drawNewBus);
-	BusHelper.addTravelListener(travelListener);
 	BusHelper.start();
 
 	clearInterval(loop);
+
 	loop = setInterval(function () {
 		BusHelper.get(selectedLine, function(err, data){
 			if(err){
@@ -159,15 +157,6 @@ $(document).ready(function(){
 			}
 		});
 	}, 150);
-
-
-	function findABus() {
-    //busnumber = $("#busnumber").val();
-    removeAllBuses();
-    executeGetBus(busnumber);
-}
-
-
-
-
+	
+	console.log(offers);
 });
