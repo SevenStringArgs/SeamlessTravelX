2bd2df4ba2253e1edd6bfbdec815234d7eb8cd56
unknown <andreas.lundsten@gmail.com>
2015-04-11 16:29:09 +0200

    added timer for test on card page

--- js/myCards.js
+++ js/myCards.js
@@ -1,7 +1,5 @@
 $(document).ready(function(){
-    
-   
-    
+    var onBus = false;
     var cards = [{id:0, type:'cash',value:'250'},{id:1,type:'period',value:'5'}];
 	console.log(cards);
     $.each(cards, function(key,card){
@@ -20,16 +18,42 @@ $(document).ready(function(){
     $("#enterButton").click(function(){
         $(".sucess-message").show();
         $(".message-thick").text("Card Scanned!");
+        timerFunction(60);
+        onBus = true;
     });
     $("#button-accept").click(function(){
         $("#successModal").modal();
+
+        
     })
     $("#exitButton").click(function(){
         $(".sucess-message").show();
         $(".message-thick").text("Bus Exited!");
+        onBus = false;
     });
     $("#menu-toggle").click(function(e) {
         e.preventDefault();
         $("#wrapper").toggleClass("toggled");
     });
+    function timerFunction(secondsToDestination) {
+        var seconds = secondsToDestination;
+        var refreshIntervalId = setInterval(function(){
+            var minutes =seconds/60
+            var timestring ='';
+            if (minutes >5){
+                timestring = 'Min: ' + parseInt(minutes);
+            }
+            else if(minutes > 1){
+                timestring = 'Min: ' + parseInt(minutes) + ' Sec: ' + seconds%60;
+            }
+            else timestring = 'Sec: ' + seconds;
+            console.log(onBus);
+            $('.information-bar').html(timestring);
+            seconds -= 1;
+            if (onBus==false){
+                clearInterval(refreshIntervalId);
+                 $('.information-bar').html("klivit av");
+            } 
+        }, 1000);
+    }
 });
